abb6b21dc7a14aa9a727d813847abee0a233acf5
周博 <zhoubo@reignwood.com>
2020-02-24 23:45:27 +0800

    init

---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+ * @Description: 
+ * @version: 
+ * @Author: zhoubo
+ * @Date: 2020-02-24 22:30:46
+ * @LastEditors: zhoubo
+ * @LastEditTime: 2020-02-24 23:22:21
+ * @FilePath: ~@index.js
+ */
+const express = require('express')
+const multer  = require('multer')
+const cors = require('cors')
+const upload = multer({ dest: 'uploads/' })
+const p = require('path')
+
+const app = express()
+
+app.options('/upload', cors())
+app.put('/upload', cors(), upload.single('file'), function (req, res, next) {
+  res.json({key: req.file.filename})
+})
+app.get('/upload/:key', cors(), function(req, res, next){
+  res.sendFile(`uploads/${req.params.key}`, {
+    root: __dirname,
+    headers:{
+      'Content-Type': 'image/jpeg',
+    },
+  }, (error)=>{
+    if(error){
+      res.status(404).send('Not found')
+    }
+  })
+})
+var port = process.env.port || 3000
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!')
+})
